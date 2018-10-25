51fabea162804d849b972ba539758fdbad9e3010
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2018-10-25 22:57:57 +0900

    bin/hyouka.jsに着手

--- bin/hyouka.js
+++ bin/hyouka.js
@@ -1,4 +1,23 @@
 #!/usr/bin/env node
 'use strict';
 
+"use strict";
+
+const fs = require('fs'),
+  expect = require('expect.js');
+
+const Monad = require('../lib/monad'),
+  Maybe = Monad.Maybe,
+  Reader = Monad.Reader,
+  Parser = Monad.Parser,
+  Cont = Monad.Cont,
+  ID = Monad.ID;
+
+const Env = require("../lib/env.js"),
+  Exp = require("../lib/exp.js"),
+  Semantics = require("../lib/semantics.js"),
+  Interpreter = require("../lib/interpreter.js");
+
 console.log("this is hyouka")
+
+
