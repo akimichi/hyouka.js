df4a7818dc067cf2ac24bb46db96ae6682e88a9c
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2018-11-01 21:16:02 +0900

    bin/hyouka.jsで実験

--- bin/hyouka.js
+++ bin/hyouka.js
@@ -1,23 +1,56 @@
 #!/usr/bin/env node
 'use strict';
 
-"use strict";
-
 const fs = require('fs'),
   expect = require('expect.js');
 
 const Monad = require('../lib/monad'),
+  ID = Monad.ID,
   Maybe = Monad.Maybe,
   Reader = Monad.Reader,
   Parser = Monad.Parser,
   Cont = Monad.Cont,
-  ID = Monad.ID;
+  IO = Monad.IO;
+
+const kansuu = require('kansuu.js'),
+  array = kansuu.array;
 
 const Env = require("../lib/env.js"),
   Exp = require("../lib/exp.js"),
   Semantics = require("../lib/semantics.js"),
   Interpreter = require("../lib/interpreter.js");
 
-console.log("this is hyouka")
 
+const prompt = (prefix) => {
+  return IO.putArray(array.fromString(prefix));
+};
 
+const promptAction = prompt("\n  >") 
+
+// IO.run(IO.flatMap(promptAction)(_ => {
+//   return IO.done(_);
+// }))
+
+const repl = () => {
+  const actions = (action) => {
+    return IO.flatMap(action)(_ => {
+      return IO.done(_)
+    });
+  };
+  return Cont.callCC((k) => {
+    return k(actions(promptAction))
+  })
+};
+IO.run(repl()(Cont.stop))
+
+      // var buffer = "";
+      // return Cont.callCC(k => {
+      //   return IO.flatMap(IO.getChar())(inputChar => {
+      //     if(inputChar === '\n') {
+      //       return IO.unit(escape(buffer));
+      //     } else {
+      //       buffer = `${buffer}inputChar`
+      //     }
+      //   })
+      // })
+// IO.run(promptAction("prompt>"))(Cont.stop);
