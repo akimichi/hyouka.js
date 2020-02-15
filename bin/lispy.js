20a6990857c623b8c10df68162bb146d37f161ce
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2020-02-15 17:23:36 +0900

    add bin/lispy.js

---
+++ bin/lispy.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs'),
+  expect = require('expect.js');
+
+const kansuu = require('kansuu.js'),
+  pair = kansuu.pair,
+  array = kansuu.array;
+
+const Monad = require('../lib/monad'),
+  Maybe = Monad.Maybe,
+  Reader = Monad.Reader,
+  Parser = Monad.Parser,
+  Cont = Monad.Cont,
+  IO = Monad.IO,
+  ID = Monad.ID;
+
+const Env = require("../lib/env.js"),
+  Exp = require('../lib/exp.js');
+
+const inputAction = (prompt) => {
+  const readlineSync = require('readline-sync');
+  return IO.unit(readlineSync.question(prompt));
+};
+
+
+
+//
+//
+// repl:: Env -> Cont[IO]
+const repl = (environment) => {
+  const Semantics = require('../lib/semantics.js');
+  const Interpreter = require("../lib/interpreter.js"),
+    Evaluator = Interpreter(Syntax.expression, Semantics.evaluator);
+
+  return Cont.callCC(exit => {
+    // loop:: Null -> IO
+    const loop = () => {
+      return IO.flatMap(inputAction("\ncalc> "))(inputString  => {
+        return IO.flatMap(IO.putString(inputString))(_ => {
+          if(inputString === 'exit') {
+            return exit(IO.done(_));
+          } else {
+            return Maybe.match(Cont.eval(Evaluator(environment)(inputString)),{
+              nothing: (message) => {
+                return IO.flatMap(IO.putString(`\nnothing: ${message}`))(_ => {
+                  return loop(); 
+                });
+              },
+              just: (value) => {
+                return IO.flatMap(IO.putString(`\n${value}`))(_ => {
+                  return loop(); 
+                });
+              }
+            })
+          }
+        });
+      });
+    };
+    return Cont.unit(loop())
+  });
+};
+
+IO.run(Cont.eval(repl(Env.prelude())))
+
