9418b3f957ed99cf73667c8e7706645ac89b82da
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2023-04-07 18:50:07 +0900

    lispy用Interpreterに不具合あり

---
+++ test/lib/lang/lispy/interpreter.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const fs = require('fs'),
+  expect = require('expect.js');
+
+const kansuu = require('kansuu.js'),
+  array = kansuu.array,
+  pair = kansuu.pair;
+
+const Monad = require('../../../../lib/monad'),
+  Maybe = Monad.Maybe,
+  Reader = Monad.Reader,
+  Parser = Monad.Parser,
+  Cont = Monad.Cont,
+  ID = Monad.ID;
+
+
+// ### Interpreterのテスト
+describe("Interpreterをテストする",() => {
+  const Env = require("../../lib/env.js"),
+    Exp = require("../../lib/exp.js"),
+    Syntax = require("../../lib/syntax.js"),
+    Semantics = require("../../lib/semantics.js"),
+    Interpreter = require("../../lib/interpreter.js");
+
+})
