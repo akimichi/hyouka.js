1b6b5b001a22b782fe416b0347af76bf308a5e00
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2023-04-07 10:49:46 +0900

    env.test.jsを準備した

---
+++ test/lib/lang/lispy/env.test.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const fs = require('fs'),
+  util = require('util'),
+  expect = require('expect.js');
+
+const kansuu = require('kansuu.js'),
+  array = kansuu.array,
+  pair = kansuu.pair;
+
+const Monad = require('../../../../lib/monad'),
+  Reader = Monad.Reader,
+  Maybe = Monad.Maybe,
+  ST = Monad.ST;
+
+// ### 環境のテスト
+describe("lispy環境をテストする",() => {
+
+  const Env = require("../../../../lib/env.js");
+  const environment = require("../../../../lib/lang/lispy/env.js");
+
+  it("Env.emptyをテストする",(done) => {
+    Maybe.match(Env.lookup('a')(environment),{
+      nothing: (_) => {
+        expect(true).to.be.ok();
+      },
+      just: (value) => {
+        expect().fail();
+      }
+    })
+    done();
+  });
+});
+
